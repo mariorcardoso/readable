d38a9eebf0a0e7ca005db6e4abf2ccff11991b80
Mario Cardoso <cardosomarioar@gmail.com>
2017-10-24 22:26:16 +0100

    create react app; add bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
