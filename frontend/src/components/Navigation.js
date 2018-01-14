aff1deb2edb5a9dee87366e525b452cfe31d9f90
Mario Cardoso <cardosomarioar@gmail.com>
2018-01-14 11:34:41 +0000

    refactor navigation to a stateless component

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
-class Navigation extends Component {
-  render() {
-    return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/">Readable</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="/">All</NavItem>
-          <NavItem eventKey={2} href="/react">React</NavItem>
-          <NavItem eventKey={3} href="/redux">Redux</NavItem>
-          <NavItem eventKey={4} href="/udacity">Udacity</NavItem>
-        </Nav>
-      </Navbar>
-    )
-  }
-}
+const Navigation =  () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">Readable</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem eventKey={1} href="/">All</NavItem>
+      <NavItem eventKey={2} href="/react">React</NavItem>
+      <NavItem eventKey={3} href="/redux">Redux</NavItem>
+      <NavItem eventKey={4} href="/udacity">Udacity</NavItem>
+    </Nav>
+  </Navbar>
+)
 
 export default Navigation;
